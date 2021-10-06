182828974f5b93f941d9c334a475851bd7d51393
maxdearruda61 <glogloarruda186@gmail.com>
2021-10-06 16:38:29 -0300

    exercícios de operadores, variáveis e arrays

---
+++ modulo-1/operadores/index.js
@@ -0,0 +1,39 @@
+// 1 //
+const minhaidade = prompt("Qual é o sua idade ?")
+const amigo = prompt ("Qual é a idade do seu melhor amigo ?")
+const amiga = prompt ("Qual é a idade do sua melhor amiga ?")
+let maior = minhaidade > amigo
+console.log("Sua idade é maior que do seu melhor amigo ?", maior )
+let diferenca = minhaidade - amigo 
+console.log ("Qual é diferenca de idade entre voces ?" , diferenca )
+//2 //
+const par = prompt ( "Insira  um numero par")
+const resultado = par / 2
+console.log (resultado)
+// na divisao sempre fica meteda do numero,se colocar um nume impar deixa o numero com virgula //
+//3//
+const idademeses = minhaidade * 12
+const idadedias = minhaidade * 365 *12 
+const idadessegundos = minhaidade * 86400 * 365 * 12
+console.log ("messes,dias,segundos" , idademeses , idadedias , idadessegundos )
+//4 //
+const primeiraVariavel = prompt("Primeira variável:");
+const segundaVariavel = prompt("Segunda variável:");
+
+const resultado2 = primeiraVariavel > segundaVariavel;
+console.log("O primeiro número é maior ao segundo?", resultado2);
+
+const resultado3 = primeiraVariavel === segundaVariavel;
+console.log("O primeiro número é igual do segundo?", resultado3);
+const divisao2 = primeiraVariavel / segundaVariavel
+
+const resultado4 = primeiraVariavel > segundaVariavel;
+console.log("O primeiro número é divisivel pelo segundo?", resultado4);
+
+const resultado5 = primeiraVariavel < segundaVariavel;
+console.log("O segundo numero é divisivel pelo primeiro?", resultado5);
+//Desafio //
+
+
+
+
---
+++ modulo-1/strings-arrays/index.js
@@ -0,0 +1,6 @@
+const nomeDoUsuario = prompt("Qual seu nome?")
+const emailDoUsuario = prompt ("Qual é seu email?")
+console.log (nomeDoUsuario)
+console.log (emailDoUsuario)
+const frase = " O e-mail " + emailDoUsuario + " foi cadastrado com sucesso. Seja bem-vinda(o), " + nomeDoUsuario + " ! "
+console.log = (frase)
